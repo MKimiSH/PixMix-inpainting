3dfd862859cd88f3dac47a4f5c741418254cd445
zhangyu94 <463568697@qq.com>
2016-12-26 22:48:31 +0800

    remove

--- libs/meanfilter_smallst.m
+++
@@ -1,7 +0,0 @@
-function RGB_Average  = meanfilter_smallst(img, num)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
- average = fspecial('average',num);
- RGB_Average = imfilter(img, average);
-end
-
--- libs/mf_smallst.m
+++
@@ -1,7 +0,0 @@
-function rgb = mf_smallst(m1, m2, i, j)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    rgb = reshape((m1(i,j,:)*2 + m2(i,j,:))/3,1,3);
-
-end
-
